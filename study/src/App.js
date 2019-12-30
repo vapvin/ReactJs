9d5a1ebdbbf7d0d2e710ac849be9e9fb9a761062
vapvin <tpehdj12@daum.net>
2019-12-30 11:58:14 +0900

    React ES6+

--- study/src/App.js
+++ study/src/App.js
@@ -2,9 +2,29 @@ import React from 'react';
 import Hello from './Hello'
 
 
+const data = [
+  {
+    title: 'Node',
+    value: 0
+  },
+  {
+    title: 'React',
+    value: 1
+  }
+]
+
 function App() {
   return (
-    <Hello />
+    <div>
+      <h1>{
+          data.map(_ => (
+            <>
+              <p>{_.title}</p>
+              <p>{_.value}</p>
+            </>
+          ))
+        }</h1>
+    </div>
   );
 }
 
