0c48120ee612071dec5fb80da8d42ea8749d4994
vapvin <tpehdj12@daum.net>
2019-12-30 14:15:54 +0900

    Class Component

--- study/src/App.js
+++ study/src/App.js
@@ -2,30 +2,27 @@ import React from 'react';
 import Hello from './Hello'
 
 
-const data = [
-  {
-    title: 'Node',
-    value: 0
-  },
-  {
-    title: 'React',
-    value: 1
+class LifeCycle extends React.Component {
+  constructor() {
+    super(props)
   }
-]
 
-function App() {
-  return (
-    <div>
-      <h1>{
-          data.map(_ => (
-            <>
-              <p>{_.title}</p>
-              <p>{_.value}</p>
-            </>
-          ))
-        }</h1>
-    </div>
-  );
+  componentWillMount() {
+    
+  }
+
+  render() {
+    return(<></>)
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillUnmount() {
+    
+  }
+
 }
 
 export default App;
