40cbb1dee3aae63a54087562976a56217e5280a5
vapvin <tpehdj12@daum.net>
2020-01-01 00:25:20 +0900

    Install Redux

--- study/src/App.js
+++ study/src/App.js
@@ -1,87 +1,29 @@
 import React, { useState } from 'react';
 
-const Todo = ({ todo, index, completeTodo, removeTodo}) => {
+const Commponent = props => <div className={props.name}>{props.children}</div>
+
+
+const App = (props) => {
   return (
-    <>
-      <div 
-        className="todo"
-        style={{textDecoration: todo.isCompleted ? 'line-through' : ''}}
-      >
-        {todo.text}
-      </div>
-      <div>
-        <button onClick={() => completeTodo(index)}>Complete</button>
-        <button onClick={() => removeTodo(index)}>X</button>
-      </div>
-    </>
-  )
-}
+    <Commponent name="comment">
+        <Commponent name="user">
+          <img
+            src={props.author.avatarUrl}
+            alt={props.author.name}
+          />
+          <Commponent name='name'>
+            {props.author.name}
+          </Commponent>
+        </Commponent>
 
-
-
-const TodoForm = ({ addTodo }) => {
-  const [value, setValue] = useState('')
-  const handleSubmit = e => {
-    e.preventDefault();
-    if(!value) return
-    addTodo(value)
-    setValue('')
-  }
-  return (
-    <form onSubmit={handleSubmit}>
-      <input 
-        type="text"
-        className="input"
-        value={value}
-        onChange={e => setValue(e.target.value)}
-      />
-    </form>
-  )
-}
-
-const App = () => {
-  const [todo, setTodo] = useState([])
-  const addTodo = text => {
-    const newTodos = [...todo, { text }]
-    setTodo(newTodos)
-    console.log(todo)
-  }
-
-  const completeTodo = index => {
-    const newTodos = [...todo]
-    newTodos[index].isCompleted = true
-    setTodo(newTodos)
-  }
-
-  const removeTodo = index => {
-    const newTodos = [...todo]
-    newTodos.splice(index, 1)
-    setTodo(newTodos)
-  }
-  
-  return (
-    <>
-      <div className="app"  style={{margin: "150px 800px"}}>
-        <div className="todo_list">
-
-          {
-            todo.map((item, index) => (
-              <Todo 
-                key={item}
-                index={index}
-                todo={todo}
-                completeTodo={completeTodo}
-                removeTodo={removeTodo}
-              />
-            ))
-          }
-
-          <TodoForm addTodo={addTodo} />
-          
-        </div>
-      </div>
-    </>
-  )
+        <Commponent name='Comment-text'>
+          {props.text}
+        </Commponent>
+        <Commponent name="Content-date">
+          {formatDate(props.date)}
+        </Commponent>
+    </Commponent>
+  );
 }
 
 export default App;
---
+++ study/src/Comment.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Comment = props => <div></div>
+
+export default Comment
