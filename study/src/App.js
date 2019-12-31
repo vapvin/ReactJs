47219d11daf968215b6bc019db34581ca7b4815d
vapvin <tpehdj12@daum.net>
2019-12-31 17:06:32 +0900

    Set State use Example

--- study/src/App.js
+++ study/src/App.js
@@ -1,17 +1,34 @@
 import React from 'react';
+import Timer from './State'
 
-function App() {
-
-  const handleClick = (par) => {
-    console.log(`${par} is clicked`)
+class Timer extends React.Component {
+  constructor(props) {
+    this.state = {
+      time: new Date()
+    }
   }
-  
-  return (
 
-      <button onClick={() => handleClick('test')}>This is a button</button>
+  componentDidMount() {
+    this.tick()
+  }
 
-  )
+  tick() {
+    this.setState({
+      time: new Date()
+    }, () => {
+        console.log(this.state)
+    })
+  }
+
+  render() {
+    const { time } = this.state
+
+    return (
+      <div>
+        {time.date.toLocaleTimeString()}
+      </div>
+    )
+  }
 }
-
 
 export default App;
