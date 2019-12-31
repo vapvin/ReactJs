bb909bed5b2a9790c93ebca4652284708b77733e
vapvin <tpehdj12@daum.net>
2019-12-31 17:56:08 +0900

    Effect Hooks

--- study/src/App.js
+++ study/src/App.js
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [data, setData] = useState({ hits: [] })
+  const [query, setQuery] = useState('react')  
+
+  useEffect(() => {
+    
+    return () => {
+      
+    };
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={() => setCount(count+1)}>Hooks is very Good!</button>{count}
-      </header>
-    </div>
+    <>
+      <input value={query} onChange={e => setQuery(e.taeget.value)} />
+      <ul>
+        {data.hits.map(item => (
+          <li>{item.title}</li>
+        ))}
+      </ul>
+    </>
   )
 }
 
