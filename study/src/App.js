2fcf6b539fee1be590139df9923d3a3e574db5e5
vapvin <tpehdj12@daum.net>
2020-01-02 17:16:32 +0900

    Install Styled Components

--- study/src/App.js
+++ study/src/App.js
@@ -1,86 +1,8 @@
-import React, { useState } from 'react';
-
-const Todo = ({ todo, index, complete, remove}) => {
-  return (
-    <>
-      <div 
-        className="todo"
-        style={{textDecoration: todo.isCompleted ? 'line-through' : ''}}
-      >
-        {todo.text}
-      </div>
-      <div>
-        <button conClick={() => complete(index)}>Complete</button>
-        <button conClick={() => remove(index)}>X</button>
-      </div>
-    </>
-  )
-}
-
-
-
-const TodoForm = ({ addTodo }) => {
-  const [value, setValue] = useState('')
-  const handleSubmit = e => {
-    e.preventDefault();
-    if(!value) return
-    addTodo(value)
-    setValue('')
-  }
-  return (
-    <form onSubmit={handleSubmit}>
-      <input 
-        type="text"
-        className="input"
-        value={value}
-        onChange={e => setValue(e.target.value)}
-      />
-    </form>
-  )
-}
+import React from 'react';
 
 const App = () => {
-  const [todo, setTodo] = useState([])
-  const addTodo = text => {
-    const newTodos = [...todo, { text }]
-    setTodo(newTodos)
-  }
-
-  const complete = index => {
-    const newTodos = [...todo]
-    newTodos[index].isCompleted = true
-    setTodo(newTodos)
-  }
-
-  const remove = index => {
-    const newTodos = [...todo]
-    newTodos.splice(index, 1)
-    setTodo(newTodos)
-  }
+  return ()
   
-  return (
-    <>
-      <div className="app">
-        <div className="todo_list">
-
-          {
-            todo.map((item, index) => (
-              <Todo 
-                key={item}
-                index={index}
-                todo={todo}
-                complete={complete}
-                remove={remove}
-              />
-            ))
-          }
-
-          <TodoForm addTodo={addTodo} />
-          
-        </div>
-      </div>
-    </>
-  )
 }
 
 export default App;
