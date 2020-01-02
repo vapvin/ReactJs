de2357b72251708a42c3d34829b8ca2b04f15f1d
vapvin <tpehdj12@daum.net>
2020-01-02 17:23:11 +0900

    Styled-Components Styled Function

--- study/src/App.js
+++ study/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Component = styled.div`
+  display:block;
+`
 
 const App = () => {
-  return ()
+  return (
+    <Component />
+  )
   
 }
 
