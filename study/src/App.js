1ea4f7bd731395d3c7312952a153f4b0d8802375
vapvin <tpehdj12@daum.net>
2020-01-02 14:37:26 +0900

    This Todo List do not get text

--- study/src/App.js
+++ study/src/App.js
@@ -1,29 +1,86 @@
 import React, { useState } from 'react';
 
-const Commponent = props => <div className={props.name}>{props.children}</div>
-
-
-const App = (props) => {
+const Todo = ({ todo, index, complete, remove}) => {
   return (
-    <Commponent name="comment">
-        <Commponent name="user">
-          <img
-            src={props.author.avatarUrl}
-            alt={props.author.name}
-          />
-          <Commponent name='name'>
-            {props.author.name}
-          </Commponent>
-        </Commponent>
+    <>
+      <div 
+        className="todo"
+        style={{textDecoration: todo.isCompleted ? 'line-through' : ''}}
+      >
+        {todo.text}
+      </div>
+      <div>
+        <button conClick={() => complete(index)}>Complete</button>
+        <button conClick={() => remove(index)}>X</button>
+      </div>
+    </>
+  )
+}
 
-        <Commponent name='Comment-text'>
-          {props.text}
-        </Commponent>
-        <Commponent name="Content-date">
-          {formatDate(props.date)}
-        </Commponent>
-    </Commponent>
-  );
+
+
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState('')
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return
+    addTodo(value)
+    setValue('')
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  )
+}
+
+const App = () => {
+  const [todo, setTodo] = useState([])
+  const addTodo = text => {
+    const newTodos = [...todo, { text }]
+    setTodo(newTodos)
+  }
+
+  const complete = index => {
+    const newTodos = [...todo]
+    newTodos[index].isCompleted = true
+    setTodo(newTodos)
+  }
+
+  const remove = index => {
+    const newTodos = [...todo]
+    newTodos.splice(index, 1)
+    setTodo(newTodos)
+  }
+  
+  return (
+    <>
+      <div className="app">
+        <div className="todo_list">
+
+          {
+            todo.map((item, index) => (
+              <Todo 
+                key={item}
+                index={index}
+                todo={todo}
+                complete={complete}
+                remove={remove}
+              />
+            ))
+          }
+
+          <TodoForm addTodo={addTodo} />
+          
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
